92f654146296d21dcd35fb4b0e6b1d70e065140c
IsisVct <isabelle.vct.souza@gmail.com>
2026-06-22 20:33:51 -0300

    feat: setup inicial da arquitetura MVC

---
+++ app-bit-backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+
+const empresaRoutes = require('./routes/empresa.routes');
+const recrutadorRoutes = require('./routes/recrutador.routes');
+app.use('/api/recrutadores', recrutadorRoutes);
+app.use('/api/empresas', empresaRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API do App BiT rodando com sucesso!' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
