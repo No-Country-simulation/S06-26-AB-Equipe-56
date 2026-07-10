f7ef8736e6feeb419320d8b748ac3dc4fd775dec
Carlos Eduardo Miranda Rodrigues <kdu.mrodrigues@gmail.com>
2026-07-10 12:21:21 -0300

    add mentoria

---
+++ app-bit-backend/src/models/mentoria.model.js
@@ -0,0 +1,33 @@
+const mentores = [
+  { 
+    mentor_id: "m001", 
+    nome: "Rafael Luiz", 
+    empresa: "App BiT", 
+    cargo: "Tech Lead & Diversidade", 
+    superpoderes: ["Arquitetura de Software", "Cultura Inclusiva"], 
+    avatar_url: "https://ui-avatars.com/api/?name=Rafael+Luiz&background=0D8ABC&color=fff" 
+  },
+  { 
+    mentor_id: "m002", 
+    nome: "Isis", 
+    empresa: "App BiT", 
+    cargo: "Head de Produto ESG", 
+    superpoderes: ["Metas ESG", "Visão de Produto"], 
+    avatar_url: "https://ui-avatars.com/api/?name=Isis&background=28A745&color=fff" 
+  },
+  { 
+    mentor_id: "m003", 
+    nome: "Limongi", 
+    empresa: "App BiT", 
+    cargo: "Especialista em Infraestrutura", 
+    superpoderes: ["Performance", "Acessibilidade B2B"], 
+    avatar_url: "https://ui-avatars.com/api/?name=Limongi&background=800080&color=fff" 
+  }
+];
+
+const solicitacoesMentoria = [];
+
+module.exports = { 
+  mentores, 
+  solicitacoesMentoria 
+};
